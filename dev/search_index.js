0ee33507b8de71d64b5065c700d756957c600cc8
Documenter.jl <documenter@juliadocs.github.io>
2023-06-22 14:33:35 +0000

    build based on 253e036

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SmoQyDEAC","category":"page"},{"location":"#SmoQyDEAC","page":"Home","title":"SmoQyDEAC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SmoQyDEAC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SmoQyDEAC]","category":"page"}]
+}
