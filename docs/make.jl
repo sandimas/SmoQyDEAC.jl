03ab89f47e06e2d4bfa45a83cb91ede59d8fefd5
sandimas <james.neuhaus@gmail.com>
2023-06-22 10:52:24 -0400

    test

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,22 @@
 using SmoQyDEAC
 using Documenter
+using Literate
+
+example_names = ["fermion_greens"]#,"phonon_greens"]
+example_literate_sources = [joinpath(pkgdir(SmoQyDEAC), "docs/src/examples", name*".jl") for name in example_names]
+example_script_destinations = [joinpath(pkgdir(SmoQyDEAC), "scripts") for name in example_names]
+example_documentation_destination = joinpath(pkgdir(SmoQyDEAC), "docs/src/examples")
+example_documentation_paths = ["examples/$name.md" for name in example_names]
 
 DocMeta.setdocmeta!(SmoQyDEAC, :DocTestSetup, :(using SmoQyDEAC); recursive=true)
+
+for i in eachindex(example_names)
+    Literate.markdown(example_literate_sources[i], example_documentation_destination; 
+                      execute = false,
+                      documenter = true)
+    Literate.script(example_literate_sources[i], example_script_destinations[i])
+end
+
 
 makedocs(;
     modules=[SmoQyDEAC],
@@ -16,6 +31,8 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
+        "Examples" => example_documentation_paths,
+        "Derivations" => "derivations.md",
     ],
 )
 
