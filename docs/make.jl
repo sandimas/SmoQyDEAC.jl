22b676e6fbe049ec92cf5685050ee432284ebb73
sandimas <james.neuhaus@gmail.com>
2023-06-23 12:46:52 -0400

    ugh

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,15 @@
-include("../src/SmoQyDEAC.jl")
-using .SmoQyDEAC
+
+using SmoQyDEAC
 using Documenter
 using Literate
 
 example_names = ["fermion_greens"]#,"phonon_greens"]
-example_literate_sources = [joinpath(pkgdir(SmoQyDEAC), "docs/src/examples", name*".jl") for name in example_names]
-example_script_destinations = [joinpath(pkgdir(SmoQyDEAC), "scripts") for name in example_names]
-example_documentation_destination = joinpath(pkgdir(SmoQyDEAC), "docs/src/examples")
+example_literate_sources = ["src/examples/"*name*".jl" for name in example_names]
+example_script_destinations = ["../scripts" for name in example_names]
+example_documentation_destination = "src/examples"
 example_documentation_paths = ["examples/$name.md" for name in example_names]
+
+
 
 DocMeta.setdocmeta!(SmoQyDEAC, :DocTestSetup, :(using SmoQyDEAC); recursive=true)
 
@@ -17,16 +19,15 @@ for i in eachindex(example_names)
                       documenter = true)
     Literate.script(example_literate_sources[i], example_script_destinations[i])
 end
-
-
+# exit()
 makedocs(;
     modules=[SmoQyDEAC],
-    authors="James Neuhaus <james.neuhaus@gmail.com>",
-    repo="https://github.com/sandimas/SmoQyDEAC.jl/blob/{commit}{path}#{line}",
+    authors="James Neuhaus <jneuhau1@utk.edu>",
+    repo="https://github.com/sandimas/SmoQyDEAC.jl",
     sitename="SmoQyDEAC.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://sandimas.github.io/SmoQyDEAC.jl",
+        canonical="https://sandimas.github.io/SmoQyDEAC.jl/",
         edit_link="main",
         assets=String[],
     ),
@@ -35,9 +36,11 @@ makedocs(;
         "Examples" => example_documentation_paths,
         "Derivations" => "derivations.md",
     ],
+    draft = false
 )
 
 deploydocs(;
     repo="github.com/sandimas/SmoQyDEAC.jl",
     devbranch="main",
 )
+
